ac4a756727e10f9624dfa12b580fea0a8fdff6b2
Thijs de Zoete <thijsdezoete@gmail.com>
2014-10-13 16:27:56 +0200

    default settings

---
+++ pypiwatch/pypiwatch/settings_mail.py
@@ -0,0 +1,7 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'my gmail account'
+EMAIL_HOST_PASSWORD = 'my gmail account password'
+DEFAULT_FROM_EMAIL = 'my gmail account'
+DEFAULT_TO_EMAIL = 'to email'
